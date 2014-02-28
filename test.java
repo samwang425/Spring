38192c053cd2997c207d810674abe25ffc9d51d1
samwang425 <samwang425@gmail.com>
2014-02-28 12:28:49 +0800

    add a new repository

---
+++ test.java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Test {
+    public static void main(String[] args) { 
+        while(true){
+        Scanner scanner = new Scanner(System.in);
+        Pattern pattern = Pattern.compile("(<[^>]*>)"); 
+        System.out.print("输入你要匹配的字符串:"); 
+        Matcher matcher = pattern.matcher(scanner.nextLine()); 
+        boolean found = false; 
+        while (matcher.find()) {  
+            System.out.println("输出：" + matcher.group()); 
+            found = true; 
+        } 
+        if (!found) { 
+            System.out.println("没匹配到！"); 
+        } 
+        }
+    }
+}
